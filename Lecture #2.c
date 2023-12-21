f01c0c13c29d5c15dc4148a4f73e5fb26e8fc5c1
Nikhil_1221 <Nikhilkumarsinghpbic@gmail.com>
2023-12-21 23:40:04 +0530

    Lecture #2

---
+++ Lecture #2.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+//main()
+//{
+//	getch();
+//	printf("Hello");
+//	getch();
+//}
+
+//main(){
+//	printf("\"MySirG\"");
+//}
+
+//	==Escape Sequences==(single character)
+//	\n
+//	\t
+//	\b
+//	\"
+//	\'
+
+//main(){
+//	int x=5,y=6;
+//	printf("Value of X is %d\n",x);
+//	printf("sum of %d and %d is %d",x,y,x+y);
+//}
+
+//Write a program which take one character from keyboard
+//using getch() function and display its ASCII code
+main()
+{
+	char c;
+	printf("Enter a character : ");
+	c=getch();
+	printf("\nASCII code of %c is %d",c,c);
+}
+
+
