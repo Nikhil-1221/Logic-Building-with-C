b135a5dd6000a5fd10b5657981fa53edcada2a59
Nikhil_1221 <Nikhilkumarsinghpbic@gmail.com>
2023-12-22 23:58:25 +0530

    Lecture #4

--- Lecture #4.c
+++ Lecture #4.c
@@ -2,39 +2,39 @@
 //Swaping of values of two int type variables
 //a=10 and b=20
 
-//main()
-//{
-//	int temp,a,b;
-//	printf("Enter two number : ");
-//	scanf("%d%d",&a,&b);
-//	printf("a=%d b=%d\n",a,b);
-//	
-////	using third variable
-////	temp=a;//10
-////	a=b;//20
-////	b=temp;//10
-//
-////	using + and - variable
-////	a=a+b;//30
-////	b=a-b;//10
-////	a=a-b;//20
-//	
-////	using * and / variable
-////	a=a*b;//200
-////	b=a/b;//10
-////	a=a/b;//20
-//
-////	using XOR(^) variable
-////	a=a^b;//30
-////	b=a^b;//10
-////	a=a^b;//20
-//
-////	In single line
-////	b=-(a=b)+(a+b);
-//		//or
-//	b=(a+b)-(a=b);
-//	
-//	printf("a=%d b=%d",a,b);	
-//}
+main()
+{
+	int temp,a,b;
+	printf("Enter two number : ");
+	scanf("%d%d",&a,&b);
+	printf("a=%d b=%d\n",a,b);
+	
+//	using third variable
+//	temp=a;//10
+//	a=b;//20
+//	b=temp;//10
+
+//	using + and - variable
+//	a=a+b;//30
+//	b=a-b;//10
+//	a=a-b;//20
+	
+//	using * and / variable
+//	a=a*b;//200
+//	b=a/b;//10
+//	a=a/b;//20
+
+//	using XOR(^) variable
+//	a=a^b;//30
+//	b=a^b;//10
+//	a=a^b;//20
+
+//	In single line
+//	b=-(a=b)+(a+b);
+		//or
+	b=(a+b)-(a=b);
+	
+	printf("a=%d b=%d",a,b);	
+}
 
 
