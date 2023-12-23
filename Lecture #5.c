5022e4a8c1c196da70437f44969f38754b365bd9
Nikhil_1221 <Nikhilkumarsinghpbic@gmail.com>
2023-12-23 21:38:03 +0530

    Lecture #5

---
+++ Lecture #5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+//write a program to check wether a given number is positive or non positive
+main()
+{
+	int a;
+	printf("Enter a number : ");
+	scanf("%d",&a);
+	if(a>0)
+	{
+		printf("positive");
+	}
+	else{
+		printf("Non positive");
+	}
+}
