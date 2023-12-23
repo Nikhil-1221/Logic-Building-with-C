8c55f069de1519be63bcc74d74657c851eb5f3bb
Nikhil_1221 <Nikhilkumarsinghpbic@gmail.com>
2023-12-23 22:28:39 +0530

    Lecture #5

--- Lecture #5.c
+++ Lecture #5.c
@@ -1,15 +1,71 @@
 #include<stdio.h>
 //write a program to check wether a given number is positive or non positive
+//main()
+//{
+//	int a;
+//	printf("Enter a number : ");
+//	scanf("%d",&a);
+//	if(a>0)
+//	{
+//		printf("positive");
+//	}
+//	else{
+//		printf("Non positive");
+//	}
+//}
+
+
+//main()
+//{
+//	if(5>4)
+//	{
+//	printf("Hello\n");
+//	printf("Welcome");
+//	}
+//	else
+//	printf("Bye");
+//}
+
+
+//show blank screen due to if block is empty
+//main()
+//{
+//	if(3!=2);
+//	else
+//	printf("Bye");
+//}
+
+
+//write a programe to check wether a given number is even or odd
+//main()
+//{
+//	int a;
+//	printf("Enter a number : ");
+//	scanf("%d",&a);
+////	if(a%2==0)
+//	if(a/2*2==a)
+//	printf("Even");
+//	else
+//	printf("Odd");
+//}
+
+//main()
+//{
+//	int a;
+//	printf("Enter a number : ");
+//	scanf("%d",&a);
+//	//binary of even no ends with 0 and odd no ends with 1
+//	if(a&1)//and of a and 1 if 1 means true else false
+////	if(a%2)//Odd%2=1 means true then it goes to if block
+//	printf("Odd");
+//	else//even%2=0 means false then it goes to else block
+//	printf("even");
+//}
+
+//Add two no without using "+" operator
 main()
 {
-	int a;
-	printf("Enter a number : ");
-	scanf("%d",&a);
-	if(a>0)
-	{
-		printf("positive");
-	}
-	else{
-		printf("Non positive");
-	}
+int a,b=6,c=5;
+a=b-(-c);
+printf("%d",a);
 }
