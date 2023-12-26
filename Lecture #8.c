d29f726f70f7b7c2e2a112a1dac060c11c8e7be2
Nikhil_1221 <Nikhilkumarsinghpbic@gmail.com>
2023-12-27 01:12:49 +0530

    Lecture #8

---
+++ Lecture #8.c
@@ -0,0 +1,93 @@
+#include<stdio.h>
+//Write a program to print english alphabets in lower case
+//main()
+//{
+//	char x;
+//	for(x='a';x<='z';x++)
+//	{
+//		printf("%c\n",x);
+//	}
+//
+//}
+
+//main()
+//{
+//	int x;
+//	for(x=97;x<=122;x++)
+//	{
+//		printf("%c\n",x);
+//	}
+//
+//}
+
+//write a program to calculate sum of first N natural numbers
+//main()
+//{
+//	int i,N,sum=0;
+//	printf("Enter a number : ");
+//	scanf("%d",&N);
+//	for(i=1;i<=N;i++)
+//	{
+//		sum=sum+i;
+//	}
+//	printf("Sum of first %d natural number is %d",N,sum);
+//}
+
+//main()
+//{
+//	int i,N,sum;
+//	printf("Enter a number : ");
+//	scanf("%d",&N);
+//	for(i=1,sum=0;i<=N;sum=sum+i,i++);
+//	printf("Sum of first %d natural number is %d",N,sum);
+//}
+
+//Write a program to print N terms of the series 1 3 6 10 15.....
+//main()
+//{
+//	int i,N,sum;
+//	printf("Enter a number : ");
+//	scanf("%d",&N);
+//	for(i=1,sum=0;i<=N;i++){
+//		sum=sum+i;
+//		printf("%d ",sum);
+//	}
+//}
+
+//Write a program to print N terms of the series 1 3 6 10 15.....
+//main()
+//{
+//	int N,i,b,a=15;
+//	printf("Enter a number : ");
+//	scanf("%d",&N);
+//	for(i=0,b=6;i<N;i++,b++)
+//	{
+//		a=a+b;
+//	printf("%d ",a);
+//	}
+//}
+
+//Write a program to print N terms of the series 2 5 10 17 26 37
+main()
+{
+	int i,d,s=2,N;
+	printf("Enter a number : ");
+	scanf("%d",&N);
+	for(i=1,d=3;i<=N;i++,d++,d++)
+	{
+		printf("%d ",s);
+		s=s+d;
+	}
+	
+}
+
+//main()
+//{
+//	int i,s,n;
+//	printf("Enter a number : ");
+//	scanf("%d",&n);
+//	for(i=1;i<n;i++)
+//	{
+//		printf("%d ",i*i+1);
+//	}
+//}
