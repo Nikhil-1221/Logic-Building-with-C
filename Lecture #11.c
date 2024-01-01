9d57456f913a83ff3e8bbe1db8aafad365315f02
Nikhil_1221 <Nikhilkumarsinghpbic@gmail.com>
2024-01-02 00:40:26 +0530

    Lecture #11

--- Lecture #11.c
+++ Lecture #11.c
@@ -13,26 +13,41 @@
 //Agenda:Understand importance of mathematics in programming
 //write a program to reserve a given number
 
+//int main()
+//{
+//	int a,b,rev=0;
+//	printf("Enter a number : ");
+//	scanf("%d",&a);
+//	while(a>0){
+//	b=a%10;
+//	rev=rev*10+b;
+//	a=a/10;
+//	}
+//	printf("%d",rev);
+//}
+
+
 int main()
 {
-	int a,b,rev=0;
+	int a,b,rev=0,count=0,lock=1;
 	printf("Enter a number : ");
 	scanf("%d",&a);
 	while(a>0){
 	b=a%10;
+	if(b==0 && lock==1){
+		count++;
+	}
+	else{
+		lock=0;
+	}
 	rev=rev*10+b;
 	a=a/10;
+	}
+	while(count--){
+		printf("%c",'0');
 	}
 	printf("%d",rev);
 }
 
-//main()
-//{
-//	int i,a;
-//	printf("Enter a number : ");
-//	scanf("%d",&a)
-//	do{
-//		
-//	}
-//	while()
-//}
+
+
