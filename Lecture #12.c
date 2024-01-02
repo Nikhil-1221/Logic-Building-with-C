90f185cadc76b931d26d1482733d4a90c773dda0
Nikhil_1221 <Nikhilkumarsinghpbic@gmail.com>
2024-01-02 23:56:04 +0530

    Lecture #12

---
+++ Lecture #12.c
@@ -0,0 +1,44 @@
+//#include<stdio.h>
+//main()
+//{
+//	int a,b;
+//	for(a=1;a<=5;a++)
+//	{
+//		for(b=1;b<=2*a-1;b++)
+//			printf("%d",b);
+//		printf("\n");
+//	}
+//}
+
+//write a program to check wether a given number is Prime or not 
+//main()
+//{
+//	int a,i,count=0;
+//	printf("enter a number : ");
+//	scanf("%d",&a);
+//	for(i=1;i<=a;i++){
+//		if(a%i==0){
+//			count++;
+//		}
+//	}
+//	if(count==2){
+//		printf("Prime");
+//	}
+//	else{
+//		printf("Not prime");
+//	}
+//}
+
+//main()
+//{
+//	int i,n;
+//	printf("Enter a number : ");
+//	scanf("%d",&n);
+//	for(i=2;i<n;i++)
+//		if(n%i==0)
+//			break;
+//	if(i==n)
+//		printf("Prime");
+//	else
+//		printf("Not Prime");
+//}
