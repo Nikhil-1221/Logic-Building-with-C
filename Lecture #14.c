ce9438a1f5e7225f2080484d9b9f79729d954ab3
Nikhil_1221 <Nikhilkumarsinghpbic@gmail.com>
2024-01-07 03:39:37 +0530

    Lecture #14

--- Lecture #14.c
+++ Lecture #14.c
@@ -6,34 +6,70 @@
 //	 i	3	AB   BA
 //		4	A     A
 
-#include <ctype.h> 
-main()
-{
-	int i,j,a=65,r;
-	printf("Enter no of rows : ");
-	scanf("%d",&r);
-	for(i=1;i<=r;i++)
-	{
-		for(j=1;j<=2*r-1;j++)
-		{
-			if(j<=r+1-i || j>=r-1+i)
-			{
-				printf("%c",a);
-				if(j<r+1-i)
-				{
-					a++;
-				}
-				if(j>=r-1+i)
-					{
-						a--;
-					}
-			}
-		else
-			{
-				printf(" ");
-			}
-		}
-		a++;
-		printf("\n");
-	}
-}
+//main()
+//{
+//	int i,j,a=65,r;
+//	printf("Enter no of rows : ");
+//	scanf("%d",&r);
+//	for(i=1;i<=r;i++)
+//	{
+//		for(j=1;j<=2*r-1;j++)
+//		{
+//			if(j<=r+1-i || j>=r-1+i)
+//			{
+//				printf("%c",a);
+//				if(j<r+1-i)
+//				{
+//					a++;
+//				}
+//				if(j>=r-1+i)
+//					{
+//						a--;
+//					}
+//			}
+//		else
+//			{
+//				printf(" ");
+//			}
+//		}
+//		a++;
+//		printf("\n");
+//	}
+//}
+
+
+//main()
+//{
+//	int i,j,r;
+//	printf("Enter no of rows : ");
+//	scanf("%d",&r);
+//	for(i=1;i<=r;i++)
+//	{
+//		int a=65;
+//		for(j=1;j<=2*r-1;j++)
+//		{
+//			if(j<=r+1-i || j>=r-1+i)
+//			{
+//				printf("%c",a);
+//				if(j<r)
+//				{
+//					a++;
+//				}
+//				else
+//					{
+//						a--;
+//					}
+//			}
+//		else
+//			{
+//				printf(" ");
+//				if(j==r)
+//				{
+//					a--;
+//				}
+//			}
+//		}
+//		printf("\n");
+//	}
+//}
+
