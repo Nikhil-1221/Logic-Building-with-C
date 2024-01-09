c7cf0db7561c35e61469baf41dcad1a30d514962
Nikhil_1221 <Nikhilkumarsinghpbic@gmail.com>
2024-01-09 23:58:18 +0530

    Lecture #15

--- Lecture #15.c
+++ Lecture #15.c
@@ -1,31 +1,35 @@
 #include<stdio.h>
 //Write a program which takes month number as an input from user and display number of days in that month
-main()
-{
-	int m;
-	printf("Enter month number : ");
-	scanf("%d",&m);
-	switch(m)
-	{
-		case 1:
-		case 3:
-		case 5:
-		case 7:
-		case 8:
-		case 10:
-		case 12:
-			printf("31 Days");
-			break;
-		case 2:
-			printf("28 Days");
-			break;
-		case 4:
-		case 6:
-		case 9:
-		case 11:
-			printf("30 Days");
-			break;
-		default:
-			printf("Envalid number of month");
-	}
-}
+//main()
+//{
+//	int m;
+//	printf("Enter month number : ");
+//	scanf("%d",&m);
+//	switch(m)
+//	{
+//		case 1:
+//		case 3:
+//		case 5:
+//		case 7:
+//		case 8:
+//		case 10:
+//		case 12:
+//			printf("31 Days");
+//			break;
+//		case 2:
+//			printf("28 Days");
+//			break;
+//		case 4:
+//		case 6:
+//		case 9:
+//		case 11:
+//			printf("30 Days");
+//			break;
+//		default:
+//			printf("Invalid number of month");
+//	}
+//}
+
+
+//Write a program which takes marks obtained in an examination (out of 100) from
+//user and display the appropriate Grade
