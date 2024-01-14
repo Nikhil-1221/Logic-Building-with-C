599820851e1455a17daee2e4383ad7e3ab7d1ea8
Nikhil_1221 <Nikhilkumarsinghpbic@gmail.com>
2024-01-14 23:58:45 +0530

    Lecture #15

--- Lecture #15.c
+++ Lecture #15.c
@@ -32,3 +32,68 @@
 
 
 //Write a program which takes marks obtained in an examination (out of 100) from user and display the appropriate Grade
+//90<marks<=100 A
+//80<marks<=90 B
+//70<marks<=80 C
+//60<marks<=70 D
+//50<marks<=60 E
+//0<marks<=50 F
+
+//main()
+//{
+//	int marks;
+//	printf("Enter marks of student : ");
+//	scanf("%d",&marks);
+//	switch(marks)
+//	{
+//		case 91 ... 100:
+//			printf("GRADE A");
+//			break;
+//		case 81 ... 90:
+//			printf("GRADE B");
+//			break;
+//		case 71 ... 80:
+//			printf("GRADE C");
+//			break;
+//		case 61 ... 70:
+//			printf("GRADE D");
+//			break;
+//		case 51 ... 60:
+//			printf("GRADE E");
+//			break;
+//		case 0 ... 50:
+//			printf("GRADE F");
+//			break;
+//		default:
+//			printf("Enter valid marks");	
+//	}
+//	
+//}
+
+
+//main()
+//{
+//	int marks;
+//	printf("Enter marks : ");
+//	scanf("%d",&marks);
+//	if(marks<0 || marks>100){
+//		printf("Enter valid marks");
+//	}
+//	else if(marks>=0 && marks<=50){
+//		printf("Grade F");
+//	}
+//	else if(marks>50 && marks<=60){
+//		printf("Grade E");
+//	}
+//	else if(marks>60 && marks<=70){
+//		printf("Grade D");
+//	}
+//	else if(marks>70 && marks<=80){
+//		printf("Grade C");
+//	}
+//	else if(marks>80 && marks<=90){
+//		printf("Grade B");
+//	}
+//	else
+//		printf("Grade A");
+//}
