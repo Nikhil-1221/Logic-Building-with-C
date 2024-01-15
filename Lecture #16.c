b7a7ba8a0f38137a8f1ec6698f0319fadacab2da
Nikhil_1221 <Nikhilkumarsinghpbic@gmail.com>
2024-01-15 23:58:27 +0530

    Lecture #16

---
+++ Lecture #16.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+//Function in c
+
+//Write a function to sum of two numbers(Take nothing returns nothing)
+//void sum()
+//	{
+//		int a,b,sum;
+//		printf("Enter two numbers : ");
+//		scanf("%d %d",&a,&b);
+//		sum=a+b;
+//		printf("Sum of %d and %d is %d",a,b,sum);
+//	}
+//main()
+//{
+//	sum();
+//}
+
+
+//Write a function to sum of two numbers(Take something returns nothing)
+//void sum(int a,int b)
+//	{
+//		int sum;
+//		sum=a+b;
+//		printf("Sum of %d and %d is %d",a,b,sum);
+//	}
+//main()
+//{
+//	int x,y;
+//	printf("Enter two numbers : ");
+//	scanf("%d %d",&x,&y);
+//	sum(x,y);
+//}
+
+
+//Write a function to sum of two numbers(Take nothing returns something)
+//int sum()
+//	{
+//		int a,b;
+//		printf("Enter two numbers : ");
+//		scanf("%d %d",&a,&b);
+//		return a+b;
+//	}
+//main()
+//{
+//	int S;
+//	S=sum();
+//	printf("Sum of two no is %d",S);
+//}
+
+
+//Write a function to sum of two numbers(Take something returns something)
+//int sum(int a,int b)
+//	{
+//		return a+b;
+//	}
+//main()
+//{
+//	int x,y,S;
+//	printf("Enter two numbers : ");
+//		scanf("%d %d",&x,&y);
+//	S=sum(x,y);
+//	printf("Sum of %d and %d is %d",x,y,S);
+//}
+
+
+//Write a function to calculate area of circle (Take something returns something)
+//int area(int r)
+//	{
+//		return 3.148*r*r;
+//	}
+//main()
+//{
+//	int x,S;
+//	printf("Enter radious of circle : ");
+//		scanf("%d",&x);
+//	S=area(x);
+//	printf("Area of circle of radious %d is %d",x,S);
+//}
