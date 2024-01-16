e4d289d2ed1496891927c6fb2e4074ae6f4f91a9
Nikhil_1221 <Nikhilkumarsinghpbic@gmail.com>
2024-01-16 23:58:55 +0530

    Lecture #16

--- Lecture #16.c
+++ Lecture #16.c
@@ -64,15 +64,106 @@
 
 
 //Write a function to calculate area of circle (Take something returns something)
-//int area(int r)
+//float area(int r)
 //	{
-//		return 3.148*r*r;
+//		return 3.14*r*r;
 //	}
 //main()
 //{
-//	int x,S;
+//	int x;
+//	float s;
 //	printf("Enter radious of circle : ");
 //		scanf("%d",&x);
-//	S=area(x);
-//	printf("Area of circle of radious %d is %d",x,S);
+//		s=area(x);
+//		printf("Area of circle of radious %d is %f",x,s);
+//}
+
+//Write a function to check wether a given number is even or odd.
+//Function will return 1 if number is even and will return 0 if number is odd.
+//(Take something returns something)
+
+//int Iseven(int a)
+//{
+//	if(a%2==0)
+//	{
+//		return 1;
+//	}
+//	else
+//	return 0;
+//}
+//main()
+//{
+//	int b;
+//	printf("Enter a number : ");
+//	scanf("%d",&b);
+//	int p=Iseven(b);
+//	printf("%d",p);
+//}
+
+//int Iseven(int a)
+//{
+//	if(a%2==0)
+//		return 1;
+//		return 0;
+//}
+//main()
+//{
+//	int b;
+//	printf("Enter a number : ");
+//	scanf("%d",&b);
+//	int p=Iseven(b);
+//	printf("%d",p);
+//}
+
+
+//int Iseven(int a)
+//{
+////	return a%2==0;
+//	return !(a%2);
+//}
+//main()
+//{
+//	int b;
+//	printf("Enter a number : ");
+//	scanf("%d",&b);
+//	int p=Iseven(b);
+//	printf("%d",p);
+//}
+
+//Write a function to calculate factorial of a number.(Take something return something)
+//int factorial(int a)
+//{
+//	int i,fact=1;
+//	for(i=1;i<=a;i++)
+//	{
+//		fact=fact*i;
+//	}
+//	return fact;
+//}
+//main()
+//{
+//	int b,c;
+//	printf("Enter a number : ");
+//	scanf("%d",&b);
+//	c=factorial(b);
+//	printf("Factorial of %d is %d",b,c);
+//}
+
+//int factorial(int a)
+//{
+//	int f=1;
+//	while(a)
+//	{
+//		f=f*a;
+//		a--;
+//	}
+//	return f;
+//}
+//main()
+//{
+//	int b,c;
+//	printf("Enter a number : ");
+//	scanf("%d",&b);
+//	c=factorial(b);
+//	printf("Factorial of %d is %d",b,c);
 //}
